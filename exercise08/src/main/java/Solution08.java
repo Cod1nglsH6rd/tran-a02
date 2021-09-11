dc7c9566da8c1969925448663fb63719a073d533
Gialam <gialam.d.tran@gmail.com>
2021-09-11 18:48:24 -0400

    Updated with EX08

---
+++ exercise08/src/main/java/Solution08.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Solution08 {
+    public static void main(String[] args) {
+        /*
+        Print "How many people?"
+            Input = people
+        Print "How many pizzas do you have?"
+            Input = pizzas
+        Print "How many slices per pizza
+            Input = slicesPerPizza
+        Calculate number of slices
+            slices = slicesPerPizza * pizzas
+            piecePerPerson = slices / people
+            leftover = slices % people
+        Print "*people* with "pizzas* (*slices* slices)
+        Print "Each person gets *piecePerPerson* pieces of pizza."
+        Print "There are *leftover* leftover pieces."
+         */
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many people?");
+        int people = input.nextInt();
+
+        System.out.println("How many pizzas do you have?");
+        int pizzas = input.nextInt();
+
+        System.out.println("How many slices per pizza?");
+        int slicesPerPizza = input.nextInt();
+
+        int slices = slicesPerPizza * pizzas;
+        int piecesPerPerson = slices/people;
+        int leftover = slices % people;
+        System.out.println(people + " with " + pizzas + " (" + slices + " slices)");
+        System.out.println("Each person gets " + piecesPerPerson + " pieces of pizza.");
+        System.out.println("There are " + leftover + " leftover pieces.");
+
+
+    }
+}
